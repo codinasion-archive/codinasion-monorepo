7d58ea4db562561f20e95942a7d34ee6b7f2a8a1
rsitters <robinsitters@gmail.com>
2023-06-15 09:44:45 +0200

    Changed comments

--- program/program/print-string-of-ascii-values/PrintStringOfAsciiValues.cs
+++ program/program/print-string-of-ascii-values/PrintStringOfAsciiValues.cs
@@ -4,31 +4,31 @@ public class PrintStringOfAsciiValues
 {
     public static void Main()
     {
-        // Prompt the user to enter ASCII values separated by spaces
+        //Prompts user to enter ASCII values separated by spaces
         Console.WriteLine("Enter the ASCII values separated by spaces:");
 
-        // Read the user input
+        //Reads user input
         string input = Console.ReadLine();
 
-        // Split the input into an array of ASCII values
+        //Splits input into an array of ASCII values
         string[] asciiValues = input.Split(' ');
 
-        // Iterate over each ASCII value in the array
+        //Iterates over each ASCII value in the array
         foreach (string asciiValue in asciiValues)
         {
-            // Try to parse the ASCII value as an integer
+            //Tries to parse the ASCII value as an integer
             int value;
             if (int.TryParse(asciiValue, out value))
             {
-                // Convert the integer value to its corresponding character
+                //Converts the integer value to its corresponding character
                 char character = (char)value;
 
-                // Print the character
+                //Prints the character
                 Console.Write(character);
             }
         }
 
-        // Print a new line after printing the characters
+        //Prints a new line after printing the characters
         Console.WriteLine();
     }
 }
