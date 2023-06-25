c1eee7f7804d157c5ab3880f8e31a69fbfed78ae
LinoyYarkoni <linoyarkoni20@gmail.com>
2023-06-25 15:09:16 +0300

    C# Convert Decimal to Hexadecimal

---
+++ program/program/convert-decimal-to-hexadecimal/ConvertDecimalToHexadecimal.cs
@@ -0,0 +1,47 @@
+public static class ConvertDecimalToHexadecimal
+{
+    private static Dictionary<int, string> hexadecimalMap = new()
+    {
+        {0, "0"},
+        {1, "1"},
+        {2, "2"},
+        {3, "3"},
+        {4, "4"},
+        {5, "5"},
+        {6, "6"},
+        {7, "7"},
+        {8, "8"},
+        {9, "9"},
+        {10, "A"},
+        {11, "B"},
+        {12, "C"},
+        {13, "D"},
+        {14, "E"},
+        {15, "F"}
+    };
+
+    public static void convertDecimalToHexadecimal(int number)
+    {
+        List<int> resultToConvert = new();
+
+        while (number > 0)
+        {
+            resultToConvert.Add(number % 16);
+            number /= 16;
+        }
+
+        Console.WriteLine(createResult(resultToConvert));
+    }
+
+    private static string createResult(List<int> toConvert)
+    {
+        string result = string.Empty;
+
+        foreach (int number in toConvert)
+        {
+            result += hexadecimalMap[number];
+        }
+
+        return result;
+    }
+}
