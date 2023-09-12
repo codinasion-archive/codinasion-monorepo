45311f0d50e538fa7d0a5977eee13e69a824262f
Gabor S <126164066+g-s-eire@users.noreply.github.com>
2023-09-12 16:06:27 +0100

    Update FindTheAdjointOfAMatrix.cs
    Applied reviewer recommendations.
    
    Signed-off-by: Gabor S <126164066+g-s-eire@users.noreply.github.com>

--- program/program/find-the-adjoint-of-a-matrix/FindTheAdjointOfAMatrix.cs
+++ program/program/find-the-adjoint-of-a-matrix/FindTheAdjointOfAMatrix.cs
@@ -4,23 +4,23 @@ class FindTheAdjointOfAMatrix {
 
     /// <summary>
     /// The function that calculates the adjoint of a matrix
-    /// <summary>
-    public static int[,] GetAdjoint(int[,] matrix){
+    /// </summary>
+    public static int[,] GetAdjoint(int[,] matrix) {
 
         // Getting the size of the matrix (square matrix)
         int n = matrix.GetLength(0);
         // Creating a 2D array to store the adjoint matrix
         int[,] adjoint = new int[n, n];
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                // Getting the cofactor of tha matrix[i, j]
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                // Getting the cofactor of the matrix[i, j]
                 int[,] cofactor = GetCofactor(matrix, i, j);
                 // Calculating the sign for the current element
                 int sign = (int)Math.Pow(-1, i + j);
                 // Calculating the determinant of the cofactor
-                int determinant = GetDeterminant(cofactor, n -1);
-                // Multiplying the determinant by the sign and assign it to adjoint[j, i]
+                int determinant = GetDeterminant(cofactor, n - 1);
+                // Multiplying the determinant by the sign and assigning it to adjoint[j, i]
                 adjoint[j, i] =  sign * determinant;
             }
         }
@@ -31,19 +31,19 @@ class FindTheAdjointOfAMatrix {
 
     /// <summary>
     /// Function to calculate the cofactor of a matrix element
-    /// <summary>
-    public static int[,] GetCofactor(int[,] matrix, int row, int col){
+    /// </summary>
+    public static int[,] GetCofactor(int[,] matrix, int row, int col) {
 
         // Getting the size of the matrix (square matrix)
         int n = matrix.GetLength(0);
         // Creating a 2D array to store the cofactor matrix
-        int[,] cofactor = new int[n-1, n-1];
+        int[,] cofactor = new int[n - 1, n - 1];
 
         int rowOffset = 0;
         int colOffset = 0;
 
-        for(int i = 0; i < n; i++){
-            if(i == row){
+        for(int i = 0; i < n; i++) {
+            if(i == row) {
                 // Skipping the current row by setting rowOffset to -1
                 rowOffset = -1;
                 continue;
@@ -51,8 +51,8 @@ class FindTheAdjointOfAMatrix {
 
             colOffset = 0;
 
-            for(int j = 0; j < n; j++){
-                if(j == col){
+            for(int j = 0; j < n; j++) {
+                if(j == col) {
                     // Skipping the current column by setting colOffset to -1
                     colOffset = -1;
                     continue;
@@ -68,10 +68,10 @@ class FindTheAdjointOfAMatrix {
 
     /// <summary>
     /// Function to calculate the determinant of a matrix
-    /// <summary>
-    public static int GetDeterminant(int[,] matrix, int size){
+    /// </summary>
+    public static int GetDeterminant(int[,] matrix, int size) {
 
-        if(size == 1){
+        if(size == 1) {
             // The Base Case, returning the single element for a 1x1 matrix
             return matrix[0, 0];
         }
@@ -79,25 +79,25 @@ class FindTheAdjointOfAMatrix {
         int determinant = 0;
         int sign = 1;
 
-        for(int i = 0; i < size; i++){
-            int[,] submatrix= new int[size -1, size -1];
+        for(int i = 0; i < size; i++) {
+            int[,] submatrix = new int[size - 1, size - 1];
 
-            for(int j = 0; j < size; j++){
-                for(int k = 0; k < size; k++){
-                    if(k < i){
+            for(int j = 0; j < size - 1; j++) {
+                for(int k = 0; k < size - 1; k++) {
+                    if(k < i) {
                         // Filling the submatrix while skipping the current column 
-                        submatrix[j - 1, k] = matrix[j, k];
+                        submatrix[j, k] = matrix[j + 1, k];
                     }
-                    else if(k > i){
+                    else {
                         // Filling the submatrix while skipping the current column
-                        submatrix[j - 1, k - 1] = matrix[j, k];
+                        submatrix[j, k] = matrix[j + 1, k + 1];
                     }
                 }
             }
 
             // Calculating the determinant
             determinant += sign * matrix[0, i] * GetDeterminant(submatrix, size - 1);
-            // Toggeling the sign for the next iteration
+            // Toggling the sign for the next iteration
             sign = -sign;
         }
 
@@ -107,23 +107,23 @@ class FindTheAdjointOfAMatrix {
 
     /// <summary>
     /// Function to display the matrix in the specified format
-    /// <summary>
-    public static void DisplayMatrix(int[,] matrix){
+    /// </summary>
+    public static void DisplayMatrix(int[,] matrix) {
 
         int numRows = matrix.GetLength(0);
-        int numCols = matrix.GetLenght(1);
+        int numCols = matrix.GetLength(1);
 
         Console.Write("[");
-        for(int i = 0; i < numRows; i++){
+        for(int i = 0; i < numRows; i++) {
             Console.Write("[");
-            for(int j = 0; j < numCols; j++){
+            for(int j = 0; j < numCols; j++) {
                 Console.Write(matrix[i, j]);
-                if(j != numCols - 1){
+                if(j != numCols - 1) {
                     Console.Write(", ");
                 }
             }
             Console.Write("]");
-            if(i != numRows - 1){
+            if(i != numRows - 1) {
                 Console.Write(", ");
             }
         }
@@ -133,8 +133,8 @@ class FindTheAdjointOfAMatrix {
 
     /// <summary>
     /// Main Method
-    /// <summary>
-    public static void Main(string[] args){
+    /// </summary>
+    public static void Main(string[] args) {
 
         // The Original Matrix
         int[,] matrix = {
