37efea7a60f18b34aff7634bcce9e0b38d8d3102
einan202 <einan202@gmail.com>
2023-10-22 21:53:27 +0300

    python find sum of digits of a number

--- program/program/find-sum-of-digits-of-a-number/find_sum_of_digits_of_a_number.py
+++ program/program/find-sum-of-digits-of-a-number/find_sum_of_digits_of_a_number.py
@@ -1,5 +1,39 @@
 # This python script calculates the sum of the digits of a random number n
 
 
-def sumOfDigits(n: int) -> int:
-    return sum([int(x) for x in str(n)])
+def find_sum_of_digits_of_a_number():
+    sum_of_digits = 0
+    digits = input("Number: ")
+    for digit in digits:
+        sum_of_digits += int(digit)
+    return sum_of_digits
+
+print(f'Result: {find_sum_of_digits_of_a_number()}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def sumOfDigits(n: int) -> int:
+#     return sum([int(x) for x in str(n)])
