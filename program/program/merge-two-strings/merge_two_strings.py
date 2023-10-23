5a5c5c88e318eaa55121ee0c7e68b30137c2bf5b
Harsh Raj <harshraj8843@gmail.com>
2023-10-23 14:56:50 +0530

    Update merge_two_strings.py

--- program/program/merge-two-strings/merge_two_strings.py
+++ program/program/merge-two-strings/merge_two_strings.py
@@ -3,17 +3,9 @@
 string1 = str(input("Enter a string  : "))
 string2 = str(input("Enter another string : "))
 
+
 def mergeString(str1: str, str2: str) -> str:
-    result = ""
-    for ch1 in str1:
-        result += ch1
-    for ch2 in str2:
-        result += ch2
-    return result
+    return str1 + str2
 
 
 print("Merged String : ", mergeString(string1, string2))
-
-# Shorthand for the above function
-# def mergeString(str1, str2):
-#     return str1 + str2
