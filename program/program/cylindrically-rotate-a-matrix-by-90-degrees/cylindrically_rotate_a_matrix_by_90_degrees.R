dfd100715e29e123c32080178c4bdabb3bc75d55
Monika Tiyyagura <126177523+Monika-Tiyyagura@users.noreply.github.com>
2023-11-15 13:10:47 +0000

    made changes as per a received comment

--- program/program/cylindrically-rotate-a-matrix-by-90-degrees/cylindrically_rotate_a_matrix_by_90_degrees.R
+++ program/program/cylindrically-rotate-a-matrix-by-90-degrees/cylindrically_rotate_a_matrix_by_90_degrees.R
@@ -1,24 +1,27 @@
-cylindrical_rotate_90 <- function(matrix) {
-    result <- matrix(nrow = ncol(matrix), ncol = nrow(matrix))  # Create an empty matrix to store the result
+# Function to perform cylindrical rotation
+cylindrical_rotate <- function(matrix_input) {
+  # Get the number of rows and columns in the matrix
+  num_rows <- nrow(matrix_input)
+  num_cols <- ncol(matrix_input)
 
-    # Rotate the matrix by 90 degrees
-    for (i in 1:nrow(matrix)) {
-        result[, i] <- rev(matrix[i, ])  # Reverse each row of the original matrix and assign it to the columns of the result matrix
-    }
+  # Create an empty matrix to store the rotated result
+  rotated_matrix <- matrix(0, nrow = num_cols, ncol = num_rows)
 
-    return(result)
+  # Perform the cylindrical rotation
+  for (i in 1:num_rows) {
+    rotated_matrix[, i] <- matrix_input[num_rows - i + 1, ]
+  }
+
+  return(rotated_matrix)
 }
 
-# Define the input matrix
+# Example usage
 input_matrix <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow = 3, byrow = TRUE)
 
-# Print the original matrix
 cat("Input Matrix:\n")
 print(input_matrix)
 
-# Perform cylindrical rotation
-rotated_matrix <- cylindrical_rotate_90(input_matrix)
+rotated_matrix <- cylindrical_rotate(input_matrix)
 
-# Print the rotated matrix
-cat("\nOutput Matrix:\n")
+cat("\nRotated Matrix:\n")
 print(rotated_matrix)
