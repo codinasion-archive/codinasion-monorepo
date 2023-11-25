fa2a2d6b72ab853613ba5fdba859796aa55aa83b
Harsh Raj <harshraj8843@gmail.com>
2023-11-26 00:00:45 +0530

    Migrate to turbo@1.10.16

--- apps/codinasion/src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="w-full py-10">
-      <div className="w-10 mx-auto aspect-square border-2 !border-t-transparent border-black dark:border-white rounded-full animate-spin "></div>
-    </div>
-  );
-}
