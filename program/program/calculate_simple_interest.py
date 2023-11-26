b4b202f9be1a6391070c9a57885266918a1c699f
psmanam <pmanam@clemson.edu>
2023-11-26 05:11:22 -0500

    solved_calculating_simple_interest

---
+++ program/program/calculate_simple_interest.py
@@ -0,0 +1,6 @@
+principal = int(input("Enter principal amount:"))
+time = int(input("Enter time:"))
+rate = int(input("Enter rate of interest:"))
+
+simple_interest= (principal)*(rate)*(time)/100
+print("simple interest =", simple_interest)
