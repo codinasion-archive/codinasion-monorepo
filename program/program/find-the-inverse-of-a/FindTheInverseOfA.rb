63bb6f332aace0adb1aa22226acb466c7fa251ea
Shreyash <ssdhuma@g.clemson.edu>
2023-11-26 16:35:10 -0500

    Write a Ruby program to find the inverse of a -added

---
+++ program/program/find-the-inverse-of-a/FindTheInverseOfA.rb
@@ -0,0 +1,30 @@
+require 'matrix'
+
+def matrix_inverse(matrix)
+  a = Matrix[*matrix]
+  
+  # Check if the matrix is invertible
+  if a.det == 0
+    puts "The matrix is not invertible."
+    return
+  end
+  
+  a_inv = a.inverse
+  
+  # Print the inverse matrix with floating-point numbers
+  a_inv.to_a.each do |row|
+    row.each { |element| print "#{element.to_f} " }
+    puts
+  end
+end
+
+# Example input matrix (invertible)
+input_matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+# Find and print the inverse
+matrix_inverse(input_matrix)
+
