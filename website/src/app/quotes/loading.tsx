5e5040b90d62a54962ab5727fb12458b0c3edcbe
Harsh Raj <harshraj8843@gmail.com>
2023-12-05 23:04:38 +0530

    Update loading component

--- website/src/app/quotes/loading.tsx
+++ website/src/app/quotes/loading.tsx
@@ -1,20 +1,11 @@
-import QuoteComponent from "@/components/Quotes/quote-component";
+import QuoteLoadingComponent from "@/components/Quotes/quote-loading-component";
 
 export default function LoadingPage(): JSX.Element {
-  const QuoteData = {
-    _id: "undefined",
-    content: "Collaborate, Create, Innovate : Together with Open Source ❤️",
-    author: "Codinasion",
-    authorSlug: "codinasion",
-    length: 0,
-    tags: [],
-  };
-
   return (
     <div className="relative md:py-10 px-6 lg:px-8">
       <div className="mx-auto max-w-4xl pt-5 md:pt-10 pb-10 sm:pt-20 sm:pb-10">
         <div>
-          <QuoteComponent QuoteData={QuoteData} />
+          <QuoteLoadingComponent />
         </div>
       </div>
     </div>
