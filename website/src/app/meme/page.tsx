e7a8c696cdad1e99b82d3c37781bfe73893de145
Harsh Raj <harshraj8843@gmail.com>
2023-12-06 20:44:40 +0530

    Add Meme web page

---
+++ website/src/app/meme/page.tsx
@@ -0,0 +1,39 @@
+import Image from "@/components/Image";
+import Link from "@/components/Link";
+import PageTitle from "@/components/PageTitle";
+import { GetMemeData } from "@/data";
+
+export default async function Page(): Promise<JSX.Element> {
+  const MemeData = await GetMemeData();
+
+  return (
+    <div className="relative md:py-10 px-6 lg:px-8">
+      <PageTitle title="Meme" description="Get a Random Programming Meme" />
+      <div className="mx-auto max-w-4xl pt-5 md:pt-10 pb-10 sm:pt-20 sm:pb-10">
+        <div>
+          <Image
+            className="mx-auto rounded"
+            src={MemeData.path}
+            alt={`${MemeData.id}`}
+            unoptimized={true}
+            width={MemeData.width}
+            height={MemeData.height}
+          />
+        </div>
+      </div>
+      <div className="mt-24">
+        <span>
+          Meme API available at:{" "}
+          <Link href="/api/meme" className="underline">
+            api/meme
+          </Link>{" "}
+          ( data ) or,{" "}
+          <Link href="/api/meme/image" className="underline">
+            api/meme/image
+          </Link>{" "}
+          ( image )
+        </span>
+      </div>
+    </div>
+  );
+}
--- website/src/data/footer.ts
+++ website/src/data/footer.ts
@@ -4,6 +4,7 @@ const FooterLinksData: FooterLinkType[] = [
   { href: "/program", title: "Program" },
   { href: "/good-first-issues", title: "Good First Issues" },
   { href: "/quote", title: "Quote" },
+  { href: "/meme", title: "Meme" },
   { href: "/contributors", title: "Contributors" },
   { href: "/api", title: "API" },
   { href: "/sitemap.xml", title: "Sitemap" },
