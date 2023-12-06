20327036c2f4b64bfddc1f8a6800827fd7828750
Harsh Raj <harshraj8843@gmail.com>
2023-12-06 21:05:29 +0530

    Test: Add meme image API

---
+++ website/src/app/api/meme/image/route.ts
@@ -0,0 +1,19 @@
+import { GetMemeData } from "@/data";
+
+export const revalidate = 0;
+
+export async function GET() {
+  // Source: https://github.com/deep5050/programming-memes
+  const meme = await GetMemeData();
+
+  // Fetch image from GitHub as a Buffer
+  const image = await fetch(meme.path).then((res) => res.arrayBuffer());
+
+  // Sent image as a response
+  return new Response(image, {
+    headers: {
+      "Content-Type": "image/png",
+      "Cache-Control": "public, max-age=86400",
+    },
+  });
+}
