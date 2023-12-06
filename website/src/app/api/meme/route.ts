5bf169f4e0075fa8bfa23024d1b1ddbac18d55f2
Harsh Raj <harshraj8843@gmail.com>
2023-12-06 19:40:04 +0530

    Test: no cache API routes

--- website/src/app/api/meme/route.ts
+++ website/src/app/api/meme/route.ts
@@ -1,5 +1,7 @@
 import { GetMemeData } from "@/data";
 
+export const revalidate = 0;
+
 export async function GET() {
   // Source: https://github.com/deep5050/programming-memes
   const meme = await GetMemeData();
--- website/src/app/api/quote/route.ts
+++ website/src/app/api/quote/route.ts
@@ -1,5 +1,7 @@
 import { GetQuoteData } from "@/data";
 
+export const revalidate = 0;
+
 export async function GET() {
   // Source: https://github.com/lukePeavey/quotable
   const quote = await GetQuoteData();
