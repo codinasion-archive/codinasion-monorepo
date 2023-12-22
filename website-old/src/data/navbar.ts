976403747328036671acd13925b17f535de94566
Harsh Raj <harshraj8843@gmail.com>
2023-12-22 20:54:17 +0530

    Remove backup

--- website-old/src/data/navbar.ts
+++
@@ -1,8 +0,0 @@
-import type { NavbarLinkType } from "@/types";
-
-const NavbarLinksData: NavbarLinkType[] = [
-  { href: "/program", title: "Program" },
-  { href: "/good-first-issues", title: "Good First Issues" },
-];
-
-export { NavbarLinksData };
