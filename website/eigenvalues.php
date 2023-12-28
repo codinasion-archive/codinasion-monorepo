880b5e0e0a7a20849598cb36c7b4bb2b2ef1fea5
Parvez Khan <parvezkhan0802@gmail.com>
2023-12-28 23:30:06 +0530

    added eigen value matrix

---
+++ website/eigenvalues.php
@@ -0,0 +1,32 @@
+<?php
+
+// Install PHPMatrix library using Composer:
+// composer require jameshalsall/php-matrix
+
+require 'vendor/autoload.php';
+
+use Matrix\Matrix;
+
+function findEigenvalues($matrix)
+{
+    // Create a Matrix object from the input array
+    $matrixObj = new Matrix($matrix);
+
+    // Get the eigenvalues
+    $eigenvalues = $matrixObj->eigenvalues();
+
+    // Output the eigenvalues
+    foreach ($eigenvalues as $eigenvalue) {
+        echo $eigenvalue . PHP_EOL;
+    }
+}
+
+// Example input matrix
+$inputMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+
+// Find and display the eigenvalues
+findEigenvalues($inputMatrix);
