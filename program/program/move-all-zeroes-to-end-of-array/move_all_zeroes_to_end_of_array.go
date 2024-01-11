dbd9bea2c5a9c446a079bc8119318b0c31faef6a
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2024-01-11 01:20:59 +0000

    Update move_all_zeroes_to_end_of_array.go
    Add array length. If left without it, it would be a slice rather than an array, but would work as well.

--- program/program/move-all-zeroes-to-end-of-array/move_all_zeroes_to_end_of_array.go
+++ program/program/move-all-zeroes-to-end-of-array/move_all_zeroes_to_end_of_array.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func moveZeroesToEnd(arr []int) []int {
+func moveZeroesToEnd(arr [16]int) [16]int {
 	var nonZeroes []int
 	for _, elem := range arr {
 		if elem != 0 {
@@ -10,11 +10,11 @@ func moveZeroesToEnd(arr []int) []int {
 		}
 	}
 
-	return append(nonZeroes, make([]int, len(arr)-len(nonZeroes))...)
+	return [16]int(append(nonZeroes, make([]int, len(arr)-len(nonZeroes))...))
 }
 
 func main() {
-	var slice = []int{0, 1, 0, 0, 2, 3, 4, 0, 5, 6, 0, 7, 8, 0, 9, 0}
-	slice = moveZeroesToEnd(slice)
-	fmt.Println(slice)
+	var arr = [16]int{0, 1, 0, 0, 2, 3, 4, 0, 5, 6, 0, 7, 8, 0, 9, 0}
+	arr = moveZeroesToEnd(arr)
+	fmt.Println(arr)
 }
