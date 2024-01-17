c2322c9b5b89c5b12dcd85e24e0ce6c7672ff8d5
Mohit Kambli <mohitkambli8@gmail.com>
2024-01-17 13:27:32 +0530

    Fix #5332: Write a Dart program to implement selection sort

--- program/program/implement-selection-sort/implement_selection_sort.dart
+++ program/program/implement-selection-sort/implement_selection_sort.dart
@@ -5,13 +5,11 @@ void selectionSort(List<int> list) {
 
   for (int i = 0; i < n - 1; i++) {
     int min = i;
-
     for (int j = i + 1; j < n; j++) {
       if (list[j] < list[min]) {
         min = j;
       }
     }
-
     if (min != i) {
       int temp = list[min];
       list[min] = list[i];
