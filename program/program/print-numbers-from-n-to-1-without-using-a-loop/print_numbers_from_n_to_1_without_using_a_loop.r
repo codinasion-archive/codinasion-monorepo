217821a1fd7541c49f592dedb08f6940a1216d53
srini696 <99717636+srini696@users.noreply.github.com>
2024-02-02 16:18:37 -0500

    Written code for printing numbers from n to 1 without using loop in R language.

---
+++ program/program/print-numbers-from-n-to-1-without-using-a-loop/print_numbers_from_n_to_1_without_using_a_loop.r
@@ -0,0 +1,10 @@
+print_numbers_reverse <- function(n) {
+  if (n >= 1) {
+    print(n)
+    print_numbers_reverse(n - 1)
+  }
+}
+
+# Example usage with input 1 2 3 4 5
+input_numbers <- c(1, 2, 3, 4, 5)
+print_numbers_reverse(max(input_numbers))
