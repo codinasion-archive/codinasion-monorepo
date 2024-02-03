5e5aae8ec961ccd394f2e3c93a45952919fa2259
Baliram Singh <heyome9a@gmail.com>
2024-02-03 16:17:14 +0000

    Update print_numbers_from_n_to_1_without_using_a_loop.r

--- program/program/print-numbers-from-n-to-1-without-using-a-loop/print_numbers_from_n_to_1_without_using_a_loop.r
+++ program/program/print-numbers-from-n-to-1-without-using-a-loop/print_numbers_from_n_to_1_without_using_a_loop.r
@@ -1,9 +1,10 @@
 print_numbers_reverse <- function(n) {
   if (n >= 1) {
-    print(n)
-    print_numbers_reverse(n - 1)
+    return(c(n, print_numbers_reverse(n - 1)))
+  } else {
+    return(NULL)
   }
 }
 
-input_numbers <- c(1, 2, 3, 4, 5)
-print_numbers_reverse(max(input_numbers))
+result <- print_numbers_reverse(10)
+print(result)
