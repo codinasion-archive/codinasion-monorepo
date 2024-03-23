f43d8cb504af5b6a536097238fc7bc1881be2dd9
Carlos Tocino <carlostocinocubelo@gmail.com>
2024-03-23 19:51:41 +0100

    Adding how to run

--- program/program/convert-temperature-from-celsius-to-kelvin/convert_temperature_from_celsius_to_kelvin.cs
+++ program/program/convert-temperature-from-celsius-to-kelvin/convert_temperature_from_celsius_to_kelvin.cs
@@ -1,9 +1,14 @@
-public float c2k(float celsius)
-{
-    if (celsius < -273.15)
-    {
-        throw new InvalidOperationException("There is nothing under ABSOLUTE ZERO, my friend");
-    }
+/*
+ * To run this program without create a full .NET project, you should install dotnet-script tool globally:
+ * % dotnet tool install -g dotnet-script
+ * Then you can run the program with the following command:
+ * % dotnet script convert_temperature_from_celsius_to_kelvin.cs
+ */
+var celsius = Convert.ToDouble(Console.ReadLine());
 
-    return celsius + 273.15;
+if (celsius < -273.15)
+{
+    throw new InvalidOperationException("There is nothing under ABSOLUTE ZERO, my friend");
 }
+
+Console.WriteLine(celsius + 273.15);
