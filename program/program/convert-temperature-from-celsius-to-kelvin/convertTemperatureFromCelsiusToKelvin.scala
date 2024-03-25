92ce457ec9395994ec4245ec502bee25a6947eb6
nine03 <youngjun_10@naver.com>
2024-03-26 05:13:35 +0900

    - Change celsius input type to Double for flexible input - Update celsius value from -40.0 to -40 - Code clean up

--- program/program/convert-temperature-from-celsius-to-kelvin/convertTemperatureFromCelsiusToKelvin.scala
+++ program/program/convert-temperature-from-celsius-to-kelvin/convertTemperatureFromCelsiusToKelvin.scala
@@ -1,7 +1,6 @@
 object CelsiusToKelvin {
   def main(args: Array[String]): Unit = {
-
-    val celsius: Double = -40.0
+    val celsius: Double = -40
     val kelvin: Double = celsius + 273.15
 
     println("Input (C): " + celsius)
