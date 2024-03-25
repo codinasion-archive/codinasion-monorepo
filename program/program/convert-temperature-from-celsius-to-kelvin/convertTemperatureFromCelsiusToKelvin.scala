c77977cb68a76a0cd48970bc95e35e4490236bda
nine03 <youngjun_10@naver.com>
2024-03-26 03:53:25 +0900

    Write a Scala program to convert temperature from celsius to kelvin

---
+++ program/program/convert-temperature-from-celsius-to-kelvin/convertTemperatureFromCelsiusToKelvin.scala
@@ -0,0 +1,11 @@
+object CelsiusToKelvin {
+  def main(args: Array[String]): Unit = {
+
+    val celsius: Double = -40.0
+    val kelvin: Double = celsius + 273.15
+
+    println("Input (C): " + celsius)
+    println("Output (K): " + kelvin)
+
+  }
+}
