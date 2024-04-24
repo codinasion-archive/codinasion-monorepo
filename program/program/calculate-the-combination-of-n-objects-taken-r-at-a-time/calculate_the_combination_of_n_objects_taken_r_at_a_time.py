743811d4ab8818b6524f76aff52b2e2dbe191d2e
Greeshmanth Penugonda <79304704+Greeshmanth1@users.noreply.github.com>
2024-04-24 22:04:34 +0530

    Update calculate_the_combination_of_n_objects_taken_r_at_a_time.py

--- program/program/calculate-the-combination-of-n-objects-taken-r-at-a-time/calculate_the_combination_of_n_objects_taken_r_at_a_time.py
+++ program/program/calculate-the-combination-of-n-objects-taken-r-at-a-time/calculate_the_combination_of_n_objects_taken_r_at_a_time.py
@@ -4,11 +4,13 @@ def ncr(n, r):
     if r == 0 or r == n:
         return 1
     r = min(r, n - r)  # take advantage of symmetry
-    # ncr    =    n! / (n-r)! * r!     =     n*(n-1)*(n-2)*..*(n-r+1)*(n-r)! / (n-r)! * r!     =     (n-r)! / r!
+    # ncr    =    n! / (n-r)! * r!     =     n*(n-1)*(n-2)*..*(n-r+1)*(n-r)! / (n-r)! * r!     =     n*(n-1)*(n-2)*..*(n-r+1) / r!
     numerator = 1   # (n-r)!
     denominator = 1 # r!
     for i in range(r):
         numerator *= (n - i)
         denominator *= (i + 1)
     return numerator // denominator
-print(ncr(5,5))
+n=int(input("Enter the n value: "))
+r=int(input("Enter the r value: "))
+print("npr value is:", ncr(n,r))
