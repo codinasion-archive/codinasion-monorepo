9875563fdfd9684332014bd7495548977f27615d
Harsh Raj <harshraj8843@gmail.com>
2024-06-16 23:32:34 +0530

    Migrate website

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.wikimedia.org",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
